9555dc129263bc1647170c1da5f0dd2de67df6f3
Ankita Gulati <ankita.gulati@walmartlabs.com>
2020-10-07 00:49:46 +0530

    Adding 2 programs

---
+++ Reverse-Words.py
@@ -0,0 +1,8 @@
+def rev_sentence(sentence): 
+    words = sentence.split(' ')  
+    reverse_sentence = ' '.join(reversed(words))  
+    return reverse_sentence   
+  
+
+x = input("Enter a line :")
+print(rev_sentence(x))
