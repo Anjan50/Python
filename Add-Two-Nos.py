13e6541f2904f9182407daa1d388fba5ae29f06a
Ankita Gulati <ankita.gulati@walmartlabs.com>
2020-10-06 16:42:32 +0530

    Adding 4 python programs

---
+++ Add-Two-Nos.py
@@ -0,0 +1,9 @@
+# Take 2 nos from user 
+number1 = input("First number: ") 
+number2 = input("\nSecond number: ") 
+  
+# Adding two numbers 
+sum = float(number1) + float(number2) 
+  
+# Display the sum 
+print("The sum of {0} and {1} is {2}" .format(number1, number2, sum)) 
