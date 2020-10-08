2f571b29c09dfa8603f70d961ac638c7a1d69021
Kishan Sahu <33571071+Kishan-Sahu@users.noreply.github.com>
2020-10-08 14:42:34 +0530

    Create Check_elements_exist_in_list.py

---
+++ Lists/Check_elements_exist_in_list.py
@@ -0,0 +1,15 @@
+def check_list(c,lst):
+
+	if c in lst:
+		return True
+	return False
+
+lst=[8,9,'s','p','q',2,5,9]
+
+print('Enter the element you want to check')
+n=input()
+
+if check_list(n,lst):
+	print("Element present in the list")
+else:
+	print("Element not present in the list")
