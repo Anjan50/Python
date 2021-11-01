c347102d22873d761f2e25080344d65e67fa96c9
SatvikNanda <93463188+SatvikNanda@users.noreply.github.com>
2021-11-01 10:48:09 +0530

    sum of squares of first n natural numbers

---
+++ Basic Programs/14_sum of first squares.py
@@ -0,0 +1,11 @@
+# Python Program for Sum of squares of first n natural numbers
+
+def sum_of_squares(n):
+	l  = [i**2 for i in range(1,n+1)]
+	return sum(l)
+print("sum of squares of first n natural numbers")
+for i in range(1,101):
+	print("n={}, sum={}".format(i,sum_of_squares(i)))
+
+	
+	
