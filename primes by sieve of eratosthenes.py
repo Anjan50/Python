93a75cee16723cba13a975e88cd1bd6e1d21909e
Aatmaj-Zephyr <Aatmaj-Zephyr@users.noreply.github.com>
2022-10-03 16:36:19 +0000

    Automated autoyapf fixes

--- primes by sieve of eratosthenes.py
+++ primes by sieve of eratosthenes.py
@@ -1,11 +1,12 @@
 # Program to print prime numbers till a number
 
+
 def getPrimes(n):
     prime = [True for x in range(n)]
     p = 2
-    while p*p <= n:
+    while p * p <= n:
         if prime[p]:
-            for i in range(p*p, n, p):
+            for i in range(p * p, n, p):
                 prime[i] = False
         p += 1
     return prime
@@ -14,7 +15,7 @@ def getPrimes(n):
 # Number used in this example is 50. You can replace it with any other number as required.
 n = 50
 primes = getPrimes(n)
-print("The prime numbers less than "+str(n)+" are")
+print("The prime numbers less than " + str(n) + " are")
 for i, j in enumerate(primes):
     if j and i > 1:
         print(i)
