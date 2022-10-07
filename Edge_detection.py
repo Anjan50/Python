a107b6c88d99cacd2282bcc3d137926979cf05f0
Aatmaj-Zephyr <Aatmaj-Zephyr@users.noreply.github.com>
2022-10-07 03:30:14 +0000

    Automated autoyapf fixes

--- Edge_detection.py
+++ Edge_detection.py
@@ -2,6 +2,7 @@ import cv2
 
 import numpy as np
 
+
 def draw_contours(img, cnts):
 
     img = np.copy(img)
@@ -9,6 +10,7 @@ def draw_contours(img, cnts):
     img = cv2.drawContours(img, cnts, -1, (0, 255, 0), 2)
 
     return img
+
 
 def draw_min_rect_circle(img, cnts):
 
@@ -24,45 +26,46 @@ def draw_min_rect_circle(img, cnts):
 
         min_rect = np.int0(cv2.boxPoints(min_rect))
 
-        cv2.drawContours(img, [min_rect], 0, (0, 255, 0), 2) 
+        cv2.drawContours(img, [min_rect], 0, (0, 255, 0), 2)
 
         (x, y), radius = cv2.minEnclosingCircle(cnt)
 
-        center, radius = (int(x), int(y)), int(radius) 
+        center, radius = (int(x), int(y)), int(radius)
 
-        img = cv2.circle(img, center, radius, (0, 0, 255), 2) 
+        img = cv2.circle(img, center, radius, (0, 0, 255), 2)
 
     return img
+
 
 def draw_approx_hull_polygon(img, cnts):
 
-    
-
     img = np.zeros(img.shape, dtype=np.uint8)
 
-    cv2.drawContours(img, cnts, -1, (255, 0, 0), 2)  
+    cv2.drawContours(img, cnts, -1, (255, 0, 0), 2)
 
-    min_side_len = img.shape[0] / 32  
+    min_side_len = img.shape[0] / 32
 
-    min_poly_len = img.shape[0] / 16  
+    min_poly_len = img.shape[0] / 16
 
-    min_side_num = 3 
+    min_side_num = 3
 
-    approxs = [cv2.approxPolyDP(cnt, min_side_len, True) for cnt in cnts] 
+    approxs = [cv2.approxPolyDP(cnt, min_side_len, True) for cnt in cnts]
 
-    approxs = [approx for approx in approxs if cv2.arcLength(approx, True) > min_poly_len]  
+    approxs = [
+        approx for approx in approxs
+        if cv2.arcLength(approx, True) > min_poly_len
+    ]
 
-    approxs = [approx for approx in approxs if len(approx) > min_side_num] 
+    approxs = [approx for approx in approxs if len(approx) > min_side_num]
 
-    
-
-    cv2.polylines(img, approxs, True, (0, 255, 0), 2)  
+    cv2.polylines(img, approxs, True, (0, 255, 0), 2)
 
     hulls = [cv2.convexHull(cnt) for cnt in cnts]
 
-    cv2.polylines(img, hulls, True, (0, 0, 255), 2)  
+    cv2.polylines(img, hulls, True, (0, 0, 255), 2)
 
     return img
+
 
 def run():
 
@@ -70,8 +73,8 @@ def run():
 
     thresh = cv2.Canny(image, 128, 256)
 
-    thresh, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
+    thresh, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL,
+                                                   cv2.CHAIN_APPROX_SIMPLE)
     """
 
     [[[-1 -1 -1 -1]]] :hierarchy  # cv2.Canny()
@@ -89,22 +92,18 @@ def run():
     """
 
     imgs = [
-
-        image, thresh,
-
+        image,
+        thresh,
         draw_min_rect_circle(image, contours),
-
         draw_approx_hull_polygon(image, contours),
-
     ]
 
     for img in imgs:
 
-        
-
         cv2.imshow("contours", img)
 
         cv2.waitKey(1943)
+
 
 if __name__ == '__main__':
 
