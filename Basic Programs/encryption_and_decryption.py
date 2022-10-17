3b179b5bb127859b4cb7b96ea56bd7237265be99
Pimanus Kutragoon <pimanus1234@gmail.com>
2022-10-18 00:56:35 +0700

    Create encryption_and_decryption.py

---
+++ Basic Programs/encryption_and_decryption.py
@@ -0,0 +1,10 @@
+#use pip install cryptography before run this file
+from cryptography.fernet import fernet
+message = "hello hacktoberfest 2022"
+key = Fernet.generate_key()
+fernet = Fernet(key)
+encryptMessage = fernet.encrypt(message.encode())
+print("original message: ", message)
+print("encrypt message: ", encryptMessage)
+decryptMessage = fernet.decrypt(encryptMessage).decode()
+print("decrypted message", decryptMessage)
