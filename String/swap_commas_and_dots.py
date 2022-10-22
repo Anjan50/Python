bf5ffe46888f350967d1c1733b1c58f2236d9dbe
Aatmaj-Zephyr <Aatmaj-Zephyr@users.noreply.github.com>
2022-10-22 05:52:22 +0000

    Automated autoyapf fixes

--- String/swap_commas_and_dots.py
+++ String/swap_commas_and_dots.py
@@ -1,17 +1,17 @@
 '''             ---:: SOURCE CODE ::---             '''
-str1 = input("Enter the string: ")        # Taking input from user
+str1 = input("Enter the string: ")  # Taking input from user
 list1 = list(str1)
 str2 = ""
 
-for i in list1:       # for loop for finind and swapping the commas and dots in the given string starts here ...
-  if i==',':
-    i = '.'
-  elif i=='.':
-    i = ','
-    str2 += i       # ... for loop ends here
+for i in list1:  # for loop for finind and swapping the commas and dots in the given string starts here ...
+    if i == ',':
+        i = '.'
+    elif i == '.':
+        i = ','
+        str2 += i  # ... for loop ends here
 
-print("The given string after swapping commas and dots:--\n",str2)        # Giving output after swapping
-
+print("The given string after swapping commas and dots:--\n",
+      str2)  # Giving output after swapping
 '''
                 ---:: OUTPUT ::---
 >>> Enter the string: My name is Raj , I am from India .
