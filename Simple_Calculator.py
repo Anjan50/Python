7ca167e2bfbf96b5af362fe2d3c5f1fe39913236
Aatmaj-Zephyr <Aatmaj-Zephyr@users.noreply.github.com>
2022-10-22 19:26:46 +0000

    Automated autoyapf fixes

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -1,22 +1,29 @@
-def add(a,b):
-  return a+b
+def add(a, b):
+    return a + b
 
-def sub(a,b):
-  return a-b
 
-def mul(a,b):
-  return a*b
+def sub(a, b):
+    return a - b
 
-def divide(a,b):
-  return a/b
 
-def simpleinterest(p,r,t):
-  return(p*r*t)/100
+def mul(a, b):
+    return a * b
 
-def compoundinterest(P,R,T):
-  return P*pow((1+r/100),T)
+
+def divide(a, b):
+    return a / b
+
+
+def simpleinterest(p, r, t):
+    return (p * r * t) / 100
+
+
+def compoundinterest(P, R, T):
+    return P * pow((1 + r / 100), T)
+
 
 def squareroot(num):
-  return num **0.5
+    return num**0.5
 
-print (simpleinterest(5000,10,6))
+
+print(simpleinterest(5000, 10, 6))
