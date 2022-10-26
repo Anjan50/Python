9d36e86da7d096294ae925390943a3ecb6802134
Aatmaj-Zephyr <Aatmaj-Zephyr@users.noreply.github.com>
2022-10-26 17:36:51 +0000

    Automated autoyapf fixes

--- prime_comprehension.py
+++ prime_comprehension.py
@@ -2,8 +2,6 @@
 Programs retrieves prime number in list using comprehension technique for give n
 """
 
-N=20
-result = [  x for x in range(2, N)
-            if all(x % y != 0 for y in range(2, x))
-        ]
+N = 20
+result = [x for x in range(2, N) if all(x % y != 0 for y in range(2, x))]
 print(result)
