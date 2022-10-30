a0a0b9b6154cc96ddf1713544bfac0523c3167dd
Anshi314159 <114865290+Anshi314159@users.noreply.github.com>
2022-10-30 17:43:33 +0530

    Delete DisplayCalender.py

--- Basic Programs/DisplayCalender.py
+++
@@ -1,8 +0,0 @@
-import calendar
-
-# To take month and year input from the user
-yy = int(input("Enter year: "))
-mm = int(input("Enter month: "))
-
-# display the calendar
-print(calendar.month(yy, mm))
