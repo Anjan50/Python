92c128527b45f800642dc5cd3aaa41f0d3b481cb
Aatmaj-Zephyr <Aatmaj-Zephyr@users.noreply.github.com>
2022-10-30 13:30:26 +0000

    Automated autoyapf fixes

--- reverse_number_wo_str.py
+++ reverse_number_wo_str.py
@@ -1,11 +1,11 @@
 """
 Reverse the given integer number without converting into str
 """
-num=123
-rev=0
+num = 123
+rev = 0
 while num:
-    rem=num%10
-    rev=rev*10 + rem
-    num=num/10
+    rem = num % 10
+    rev = rev * 10 + rem
+    num = num / 10
 
-print("Reversed Number : ",rev)
+print("Reversed Number : ", rev)
--- star_pattern_representation.py
+++ star_pattern_representation.py
@@ -8,13 +8,11 @@ Make a pattern like below
 *******
 """
 
-length_star=7
+length_star = 7
 
-for i in range(1,length_star+1,2):
-    num=length_star-i
-    print(int(num/2)*" ",end="")
-    print(i*"*",end="")
-    print(int(num/2)*" ",end="")
-    print("\n",end='')
-
-
+for i in range(1, length_star + 1, 2):
+    num = length_star - i
+    print(int(num / 2) * " ", end="")
+    print(i * "*", end="")
+    print(int(num / 2) * " ", end="")
+    print("\n", end='')
